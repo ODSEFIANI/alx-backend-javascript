111436a4b09bc33a857d03f786a0434e2902ac15
OUSSAMA SEFIANI <oussama.sefiani@um5r.ac.ma>
2024-01-02 23:40:07 +0100

    2. Arrow functions

---
+++ 0x00-ES6_basic/2-arrow.js
@@ -0,0 +1,8 @@
+export default funstion getneighborhoodsList() {
+	this.sanFrancisconeighborhoods = ['SOMA', 'Union square'];
+
+	this.addNeighborhood = (newNeighborhood) => {
+		this.sanFranciscoNeighborhooods.push(NewNeighborhood);
+		return this.sanFranciscoNeighborhoods;
+w	}
+}
