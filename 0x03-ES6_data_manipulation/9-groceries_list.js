3502b579302bb641fdedadf4fbe1f15f885e8aec
OUSSAMA SEFIANI <oussama.sefiani@um5r.ac.ma>
2024-01-09 01:24:02 +0100

    9. Map data structure

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,9 @@
+export default function groceriesList() {
+    const toMap = new Map();
+    toMap.set('Apples', 10);
+    toMap.set('Tomatoes', 10);
+    toMap.set('Pasta', 1);
+    toMap.set('Rice', 1);
+    toMap.set('Banana', 5);
+    return toMap;
+}
