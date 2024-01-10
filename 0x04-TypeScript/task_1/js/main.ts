9b9cc673e8e19b397dce4058c6d90f075f775aae
ODSEFIANI <oussma.sefiani@um5r.ac.ma>
2024-01-10 18:50:12 +0100

    3. Printing teachers

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -37,3 +37,12 @@ function createTeacher({
 interface Directors extends Teacher {
   numberOfReports: number;
 }
+interface PrintTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: PrintTeacherFunction = (firstName, lastName) => {
+  const initial = firstName.charAt(0).toUpperCase();
+  const fullLastName = lastName.charAt(0).toUpperCase() + lastName.slice(1);
+  return `${initial}. ${fullLastName}`;
+};
