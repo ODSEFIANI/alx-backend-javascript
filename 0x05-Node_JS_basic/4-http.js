043c95dbfe624ea1817eec116978db30baa6f8a7
ODSEFIANI <oussma.sefiani@um5r.ac.ma>
2024-02-28 01:43:18 +0100

    4. Create a small HTTP server using Node's HTTP module

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 1245;
+const host_name = '127.0.0.1';
+const lis_port = 1245;
 
 const app = http.createServer((request, response) => {
   response.statusCode = 200;
@@ -9,7 +9,7 @@ const app = http.createServer((request, response) => {
   response.end('Hello Holberton School!');
 });
 
-app.listen(port, hostname, () => {
+app.listen(lis_port, host_name, () => {
 });
 
 module.exports = app;
